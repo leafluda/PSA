fb46c4f432064887334e230651371af5213802fa
leafluda <101168372+leafluda@users.noreply.github.com>
2023-05-20 20:58:32 +0900

    코드 간략화

--- problem_solving/week10/find_value.c
+++ problem_solving/week10/find_value.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-/* 형식문자가 종류별로 몇개인지 확인하기 위한 함수 */
+/* format_Sentence에 형식문자가 종류별로 몇개인지 확인하기 위한 함수 */
 int check_Count(char* format_Sentence, int* int_Num, int* string_Num, int* char_Num, int* format_Sequence) {
     char* sentence = format_Sentence;
     int index = 0;
@@ -29,7 +29,7 @@ int check_Count(char* format_Sentence, int* int_Num, int* string_Num, int* char_
         sentence++;
         index++;
     }
-    return 0;
+    return index;
 }
 
 /* sentence의 앞에서부터 n만큼의 문자열을 지우고 나머지 문자열을 앞으로 당기는 함수 */
@@ -45,18 +45,11 @@ void remove_Characters(char* sentence, int n) {
         // n이 0보다 큰 경우
         // n개의 문자를 제거하고 나머지 문자열을 앞으로 당김
         memmove(sentence, sentence + n, length - n + 1);
+        sentence[length - n] = '\0';
     }
 }
 
-/* str n번째 위치부터 2칸만큼의 문자열을 지우고 나머지 문자열을 앞으로 당기는 함수 */
-void remove_length_Characters(char* sentence, int n) {
-    int length = strlen(sentence);
-
-    memmove(sentence + (n), sentence + (n)+2, length - (n)-1);
-    sentence[length - 2] = '\0';
-}
-
-/* sentence에서 substring의 위치를 찾고, source의 맨앞부터 substring직전의 위치까지 destination에 복사하는 함수 */
+/* sentence에서 substring의 위치를 찾고, sentence의 맨앞부터 substring직전의 위치까지 value에 복사하는 함수 */
 void copy_Substring(char* sentence, char* value, char* substring) {
     char* position = strstr(sentence, substring); // substring의 위치를 찾음
 
@@ -91,19 +84,19 @@ void remove_Substring(char* sentence, char* substring) {
 }
 
 /* sentence에서 뒤에서 n번째의 문자가 무엇인지 반환하는 함수 */
-char get_last_Character(char* sentence, int n) {
+char get_last_Character(char* format_Sentence, int n) {
     int i = 0;
 
-    while (sentence[i] != '\0') { // \0위치를 찾음
+    while (format_Sentence[i] != '\0') { // \0위치를 찾음
         i++;
     }
     if (i > 0) { // num번째 전 위치에 있는 문자열을 반환
-        return sentence[i - n];
+        return format_Sentence[i - n];
     }
     return '\0'; // 문자열이 비어있을 경우 기본값으로 null 문자 반환
 }
 
-/* format_sentence을 이용해 value_sentence에서 format_Sequence의 순서에 맞게 형식문자에 해당하는 값을 각 자료형 배열에 저장하는 함수 */
+/* format_Sentence을 이용해 value_Sentence에서 format_Sequence의 순서에 맞게 형식문자에 해당하는 값을 각 자료형 배열에 저장하는 함수 */
 void find_Value(char* format_Sentence, char* value_Sentence, int* format_Sequence, char** s, int* i, char* c, int index) {
     char f_s[1024] = ""; // format_sentence를 저장해서 함수 내에서 사용하기 위해 선언
     char v_s[1024] = ""; // value_sentence를 저장해서 함수 내에서 사용하기 위해 선언
@@ -227,7 +220,7 @@ void find_Value(char* format_Sentence, char* value_Sentence, int* format_Sequenc
     }
 }
 
-/* 형식문장을 검사해서 예외처리 하는 함수 */
+/* format_Sentence을 검사해서 예외처리 하는 함수 */
 int sentence_Test(char* format_Sentence) {
     int i = 0;
     int count = 0;
@@ -270,7 +263,7 @@ int sentence_Test(char* format_Sentence) {
     return count;
 }
 
-/* main_sentence에서 ""안에 있는 값만 main_sentence에 다시 저장하는 함수*/
+/* sentence에서 ""안에 있는 값만 sentence에 다시 저장하는 함수*/
 void extract_quoted_Sentences(char* sentence) {
     char main_Sentence[1024];
     char* quote_Start = strstr(sentence, "\"");
@@ -290,41 +283,36 @@ int main() {
     /*--------------------------------파일 입출력----------------------------------*/
 
     FILE* file;
-    char format_Sentence[1024]; // 형식문장 저장 문자열
-    char value_Sentence[1024]; // 값문장 저장 문자열
+    char format_Sentence[1024], value_Sentence[1024]; // 형식문장 저장 문자열, 값문장 저장 문자열
 
-    file = fopen("psa.txt", "r"); // 
+    file = fopen("psa.txt", "r"); // 파일 열기
     if (file == NULL) {
         printf("파일을 열 수 없습니다.");
         return 1;
     }
 
-    fgets(format_Sentence, sizeof(format_Sentence), file);
-    fgets(value_Sentence, sizeof(value_Sentence), file);
+    fgets(format_Sentence, sizeof(format_Sentence), file); // 파일에서 첫번째 문장을 읽어옴
+    fgets(value_Sentence, sizeof(value_Sentence), file); // 파일에서 두번째 문장을 읽어옴
 
-    extract_quoted_Sentences(format_Sentence);
-    extract_quoted_Sentences(value_Sentence);
+    extract_quoted_Sentences(format_Sentence); // ""안에있는 값만 문자열로 저장
+    extract_quoted_Sentences(value_Sentence); // ""안에있는 값만 문자열로 저장
 
     printf("형식문장 : %s\n", &format_Sentence);
     printf("  값문장 : %s\n\n", &value_Sentence);
 
-    if (sentence_Test(value_Sentence) > 0) {
+    if (sentence_Test(value_Sentence) > 0) { // 형식문장을 수정시 출력
         printf("\n수정된 형식문장 : %s\n", &format_Sentence);
         printf("         값문장 : %s\n\n", &value_Sentence);
     }
 
     /*----------------------------------동적할당----------------------------------*/
 
-    int format_Sequence[100];
-    int int_Num = 0;
-    int string_Num = 0;
-    int char_Num = 0;
-
-    check_Count(format_Sentence, &int_Num, &string_Num, &char_Num, format_Sequence);
-    int index = int_Num + string_Num + char_Num;
+    int format_Sequence[100]; // 형식문자의 순서 저장
+    int int_Num = 0, string_Num = 0, char_Num = 0, j = 0; // 각 형식문자의 개수저장 , 반복문을 위한 변수 선언
+    int index = check_Count(format_Sentence, &int_Num, &string_Num, &char_Num, format_Sequence);
     char** s = (char**)malloc(string_Num * sizeof(char*)); // %s개수만큼 동적핳당
-    for (int i = 0; i < string_Num; i++) {
-        s[i] = (char*)malloc(1024 * sizeof(char)); // 각각의 문자열 크기는 1024으로 가정
+    for (j = 0; j < string_Num; j++) {
+        s[j] = (char*)malloc(1024 * sizeof(char)); // 각각의 문자열 크기는 1024으로 가정
     }
     int* i = (int*)malloc(int_Num * sizeof(int)); // %d개수만큼 동적핳당
     char* c = (char*)malloc(char_Num * sizeof(char)); //%c개수만큼 동적핳당
@@ -334,37 +322,34 @@ int main() {
     find_Value(format_Sentence, value_Sentence, format_Sequence, s, i, c, index); // 형식문자에 해당하는 값 찾기
 
     printf("int(% d개) :", int_Num); // %d에 해당하는 값 출력
-    for (int j = 0; j < int_Num; j++) {
+    for (j = 0; j < int_Num; j++) {
         if (j == (int_Num - 1)) {
             printf(" %d\n", i[j]);
-        }
-        else {
+        } else {
             printf(" %d,", i[j]);
         }
     }
     printf("char(%d개) :", char_Num); // %c에 해당하는 값 출력
-    for (int j = 0; j < char_Num; j++) {
+    for (j = 0; j < char_Num; j++) {
         if (j == (char_Num - 1)) {
             printf(" %c\n", c[j]);
-        }
-        else {
+        } else {
             printf(" %c,", c[j]);
         }
     }
     printf("string(%d개) :", string_Num); // %s에 해당하는 값 출력
-    for (int j = 0; j < string_Num; j++) {
+    for (j = 0; j < string_Num; j++) {
         if (j == (string_Num - 1)) {
             printf(" %s\n", s[j]);
-        }
-        else {
+        } else {
             printf(" %s,", s[j]);
         }
     }
 
     /*---------------------------메모리 해제 및 파일닫기--------------------------*/
 
-    for (int i = 0; i < string_Num; i++) { // 문자열에 해당하는 메모리 해제
-        free(s[i]);
+    for (j = 0; j < string_Num; j++) { // 문자열에 해당하는 메모리 해제
+        free(s[j]);
     }
     free(s); // 배열에 해당하는 메모리 해제
     free(i); // 배열에 해당하는 메모리 해제
