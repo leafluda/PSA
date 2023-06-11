dae83f574bb986736a16e02d0894206a00b9c73c
leafluda <101168372+leafluda@users.noreply.github.com>
2023-06-12 01:16:16 +0900

    주석 추가

--- problem_solving/weekEnd/Slime_Runner.cpp
+++ problem_solving/weekEnd/Slime_Runner.cpp
@@ -11,12 +11,17 @@
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false); // 표준 입출력 가속화
-    cin.tie(NULL);
+    // 표준 입출력 가속화
+    ios::sync_with_stdio(false); 
+    cin.tie(NULL); 
     cout.tie(NULL);
+
+    // 콘솔창 크기 설정
     system("mode con cols=200 lines=41");
 
+    // 게임 엔진 객체 생성
     GameEngine game;
+    // 게임 시작
     game.GameRun();
 
     return 0;
