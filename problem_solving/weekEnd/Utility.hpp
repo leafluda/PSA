a7592fa013cb9d0af0ffe1c8b3ab6cfc0954e093
leafluda <101168372+leafluda@users.noreply.github.com>
2023-06-12 01:19:26 +0900

    주석 추가

--- problem_solving/weekEnd/Utility.hpp
+++ problem_solving/weekEnd/Utility.hpp
@@ -9,6 +9,7 @@
 #include <chrono>
 #include <fstream>
 
+// 텍스트와 배경 색상 정의
 #define TextBlack "\033[30m"
 #define TextRed "\033[31m"
 #define TextGreen "\033[32m"
@@ -25,7 +26,6 @@
 #define TextLpink "\033[95m"
 #define TextLmint "\033[96m"
 #define TextWhite "\033[97m"
-
 #define BgBlack "\033[40m"
 #define BgRed "\033[41m"
 #define BgGreen "\033[42m"
@@ -42,7 +42,9 @@
 #define BgLpink "\033[105m"
 #define BgLmint "\033[106m"
 #define BgWhite "\033[107m"
+#define Reset "\033[0m"
 
+// 텍스트와 배경 색상을 결합한 매크로 정의
 #define Black TextBlack BgBlack
 #define Red TextRed BgRed
 #define Green TextGreen BgGreen
@@ -61,59 +63,60 @@
 #define White TextWhite BgWhite
 #define Text TextBlack BgWhite
 
-#define Reset "\033[0m"
-
 using namespace std;
 
-void GoToXY(int x, int y) { // 커서 위치 이동용
-
+// 커서 위치 이동 함수
+void GoToXY(int x, int y) {
     COORD Pos = { x,y };
-    //커서 이동
+    // 커서 위치 설정
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 
     CONSOLE_CURSOR_INFO ConsoleCursor;
-    ConsoleCursor.bVisible = false;
+    ConsoleCursor.bVisible = false; // 커서를 안보이게 설정
     ConsoleCursor.dwSize = 1;
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &ConsoleCursor);
 }
 
+//템플릿을 사용하여 메모리 풀을 구현해 다양한 타입의 객체를 저장가능
 template<class T>
 class MemoryPool {
+
 public:
+
     MemoryPool(int capacity) : capacity(capacity) {
-        objects.reserve(capacity);
+        objects.reserve(capacity); // capacity만큼의 공간을 미리 예약한다.
         for (int i = 0; i < capacity; ++i) {
-            objects.push_back(new T());
+            objects.push_back(new T()); // T 타입의 객체를 생성하여 objects에 추가한다.
         }
     }
 
     ~MemoryPool() {
         for (T* object : objects) {
-            delete object;
+            delete object; // objects에 들어있는 모든 객체를 삭제한다.
         }
     }
 
     T* Acquire() {
-        if (objects.empty()) {
-            Expand();
+        if (objects.empty()) { // objects가 비어있는 경우
+            Expand(); // Expand 함수를 호출하여 capacity를 늘린다.
         }
-        T* object = objects.back();
-        objects.pop_back();
-        return object;
+        T* object = objects.back(); // objects의 마지막 요소를 가리키는 포인터를 가져온다.
+        objects.pop_back(); // objects의 마지막 요소를 제거한다.
+        return object; // 가져온 객체 포인터를 반환한다.
     }
 
     void Release(T* object) {
-        objects.push_back(object);
+        objects.push_back(object); // objects에 객체 포인터를 추가한다.
     }
 
 private:
     void Expand() {
         for (int i = 0; i < capacity; ++i) {
-            objects.push_back(new T());
+            objects.push_back(new T()); // T 타입의 객체를 생성하여 objects에 추가한다.
         }
-        capacity *= 2;
+        capacity *= 2; // capacity를 두 배로 늘린다.
     }
 
-    vector<T*> objects;
-    int capacity;
+    vector<T*> objects; // T 타입의 객체 포인터를 저장하는 벡터
+    int capacity; // 메모리 풀의 용량
 };
